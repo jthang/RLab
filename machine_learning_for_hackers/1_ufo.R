b8a39dd2e0faf6000f32101c162a0363686fbab1
jthang <jensthang@gmail.com>
2015-01-11 17:25:56 +0800

    data cleaning

--- machine_learning_for_hackers/1_ufo.R
+++ machine_learning_for_hackers/1_ufo.R
@@ -1,26 +1,67 @@
 library(ggplot2)
 library(dplyr)
 library(tidyr)
-library(ggplot2)
 library(lubridate)
 
 ufo <- tbl_df(read.delim("machine_learning_for_hackers/data/ufo_awesome.tsv",
                           check.names = FALSE, header=FALSE, na.strings="", stringsAsFactors = FALSE))
+
+# rename columns
+names(ufo) <- c("DateOccurred", "DateReported",
+                "Location", "ShortDescription",
+                "Duration", "LongDescription")
+
 summary(ufo)
 head(ufo)
 View(ufo)
 str(ufo)
 
-names(ufo) <- c("DateOccurred", "DateReported",
-                "Location", "ShortDescription",
-                "Duration", "LongDescription")
+# filter only good dates rows and change the format
+ufo1 <- ufo %>%
+  filter(
+    nchar(DateOccurred) == 8, 
+    nchar(DateReported) == 8
+    ) %>%
+  mutate(
+    DateOccurred = ymd(DateOccurred),
+    DateReported = ymd(DateReported)
+    )
+ufo1
 
-ufo <- filter(ufo,
-              nchar(DateOccurred) == 8,
-              nchar(DateReported) == 8)
+# function to extract the location
+get.location <- function(l)
+{
+  split.location <- tryCatch(strsplit(l, ",")[[1]],
+                             error = function(e) return(c(NA, NA)))
+  clean.location <- gsub("^ ","",split.location)
+  if(length(clean.location) > 2)
+  {
+    return(c(NA,NA))
+  }
+  else
+  {
+    return(clean.location)
+  }
+}
 
-ufo$DateOccurred <- ymd(ufo$DateOccurred)
-ufo$DateReported <- ymd(ufo$DateReported)
+# applying the function to return list
+# use lapply and do.call function to manipulate data
+city_state <- lapply(ufo1$Location, get.location)
+location_matrix <- do.call(rbind, city_state)
 
-str(ufo)
+ufo2 <- ufo1 %>%
+  mutate(
+    USCity = location_matrix[, 1],
+    USState = location_matrix[, 2]
+    ) %>%
+  filter(
+    USState == state.abb) %>%
+  select(DateOccurred, USState)
+ufo2
+
+ggplot(ufo2, aes(x = USState)) + geom_histogram()
+
+
+
+
 
