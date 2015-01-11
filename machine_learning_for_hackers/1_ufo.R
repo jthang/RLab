077903d5d1523b3bbd9af85cd3006045b95740b1
jthang <jensthang@gmail.com>
2015-01-11 18:09:32 +0800

    completed ufo sighting

--- machine_learning_for_hackers/1_ufo.R
+++ machine_learning_for_hackers/1_ufo.R
@@ -3,7 +3,7 @@ library(dplyr)
 library(tidyr)
 library(lubridate)
 
-ufo <- tbl_df(read.delim("machine_learning_for_hackers/data/ufo_awesome.tsv",
+ufo <- tbl_df(read.delim("data/ufo_awesome.tsv",
                           check.names = FALSE, header=FALSE, na.strings="", stringsAsFactors = FALSE))
 
 # rename columns
@@ -13,14 +13,12 @@ names(ufo) <- c("DateOccurred", "DateReported",
 
 summary(ufo)
 head(ufo)
-View(ufo)
 str(ufo)
 
 # filter only good dates rows and change the format
 ufo1 <- ufo %>%
   filter(
-    nchar(DateOccurred) == 8, 
-    nchar(DateReported) == 8
+    nchar(DateOccurred) == 8, nchar(DateReported) == 8
     ) %>%
   mutate(
     DateOccurred = ymd(DateOccurred),
@@ -55,13 +53,13 @@ ufo2 <- ufo1 %>%
     USState = location_matrix[, 2]
     ) %>%
   filter(
-    USState == state.abb) %>%
+    USState == state.abb,
+    DateOccurred > "1990-01-01") %>%
   select(DateOccurred, USState)
 ufo2
 
 ggplot(ufo2, aes(x = USState)) + geom_histogram()
-
-
+ggplot(ufo2, aes(x = DateOccurred)) + geom_histogram() + facet_wrap(~ USState, ncol=5)
 
 
 
